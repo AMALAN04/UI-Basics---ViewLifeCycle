a266691c7bb8966cc7d5e0d1cf202952c6da917d
Amalan C <amalan.ee19@bitsathy.ac.in>
2022-08-24 11:04:04 +0530

    Initial Commit

---
+++ UI Basics - ViewLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Basics - ViewLifeCycle
+//
+//  Created by amalan-pt5585 on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
